5092385ee1a404030bd114a96c2e8235cb4af5e6
18612849621 <1354368242@qq.com>
2025-06-18 20:55:40 +0800

    完成大部分字符串打印的任务

--- code/v1.0.4/kernel/main.c
+++ code/v1.0.4/kernel/main.c
@@ -1,6 +1,21 @@
 #include "kernel/print.h"
 int main(void) {
-  put_char('a');
+  //   put_char(0x08);
+  put_char('k');
+  put_char('e');
+  put_char('r');
+  put_char('n');
+  put_char('e');
+  put_char('l');
+  put_char('\n');
+  put_char('\n');
+  put_char('k');
+  put_char('e');
+  put_char('r');
+  put_char('n');
+  put_char('e');
+  put_char('l');
+
   while (1)
     ;
   return 0;
