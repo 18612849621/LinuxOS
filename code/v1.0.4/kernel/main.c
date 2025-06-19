d102c8f781e9086c3afbe75efd2b81ef3e403879
18612849621 <1354368242@qq.com>
2025-06-19 20:14:28 +0800

    添加打印 int 数据 (16进制)

--- code/v1.0.4/kernel/main.c
+++ code/v1.0.4/kernel/main.c
@@ -11,8 +11,13 @@ int main(void) {
   //   put_char('2');
   //   put_char('\b');
   //   put_char('3');
-  put_str("hello, world!\n");
-  put_str("kenrel v1.0.4\n");
+  //   put_str("hello, world!\n");
+  //   put_str("kenrel v1.0.4\n");
+  put_int(0x000000010);
+  put_char('\n');
+  put_int(10);
+  put_char('\n');
+  put_int(0x12345678);
   while (1)
     ;
   return 0;
