62d9ae0c14dd78b47c3db3d661764b947b20fdcf
18612849621 <1354368242@qq.com>
2025-06-19 11:54:36 +0800

    实现字符串打印

--- code/v1.0.4/kernel/main.c
+++ code/v1.0.4/kernel/main.c
@@ -1,23 +1,18 @@
 #include "kernel/print.h"
 int main(void) {
-  //   put_char(0x08);
-  put_char('k');
-  put_char('e');
-  put_char('r');
-  put_char('n');
-  put_char('e');
-  put_char('l');
-  int i = 0;
-  for (i = 0; i < 10; i++) {
-    put_char('\n');
-  }
-  put_char('k');
-  put_char('e');
-  put_char('r');
-  put_char('n');
-  put_char('e');
-  put_char('l');
-
+  //   put_char('k');
+  //   put_char('e');
+  //   put_char('r');
+  //   put_char('n');
+  //   put_char('e');
+  //   put_char('l');
+  //   put_char('\n');
+  //   put_char('1');
+  //   put_char('2');
+  //   put_char('\b');
+  //   put_char('3');
+  put_str("hello, world!\n");
+  put_str("kenrel v1.0.4\n");
   while (1)
     ;
   return 0;
